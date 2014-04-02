739f0e470900e8a115e5367ddac0379620a8da11
bxrmbl <godzilla@bxrmbl.com>
2014-04-02 15:13:28 -0700

    new stuff

--- newstory/models.py
+++ newstory/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class NewStory(model.Models):
-  title = models.CharField(max_length=30)
+  title = models.CharField(max_length=200)
+  body = models.TextField()
+  pub_date = models.DateTimeField('date published')
+  author = models.ForeignKey(User)
+  
